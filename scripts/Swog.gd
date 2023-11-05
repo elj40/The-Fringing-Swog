dd73a762f31d54bed214ef4e30af687c31a9ce64
elj40 <elaijoubert@gmail.com>
2023-11-05 14:50:32 +0200

    Aim and swing completed

--- scripts/Swog.gd
+++ scripts/Swog.gd
@@ -1,9 +1,9 @@
 extends KinematicBody2D
 
 const GRAVITY = 300
-const TONGUE_PULL = 105
+const TONGUE_PULL = 500;
 
-export(int) var move_speed = 4000;
+export(int) var move_speed = 100;
 export(int) var jump_power = -200;
 var direction = 1;
 
@@ -16,55 +16,53 @@ func _ready():
 func _input(event):
 	if event is InputEventMouseButton:
 		if Input.is_mouse_button_pressed( 1 ):
-			$Tongue.shoot(event.position - get_viewport().size*0.5)
+			#$Tongue.shoot(event.position -get_viewport().size*0.5)
+			pass
 		else:
-			$Tongue.release()
+			#$Tongue.release()
+			pass
 
 func rotate_swog():
 	var velocity_dir = atan2(velocity.y, velocity.x)
 	var sprite_dir = velocity_dir+PI
 	if direction == -1:
 		sprite_dir= velocity_dir
+	if $Tongue.hooked:
+		var target_dir = $Tongue/Tip.global_position - global_position;
+		sprite_dir = atan2(target_dir.y, target_dir.x) + PI
+		if direction == -1:
+			sprite_dir = atan2(target_dir.y, target_dir.x)
 	$AnimatedSprite.set_rotation(sprite_dir)
 
 func _physics_process(delta):
-	velocity.x = 0
+	if Input.is_action_just_pressed("ui_left") or Input.is_action_just_pressed("ui_right"):
+		velocity.x = 0;
+		
 	var grounded = is_on_floor()
 	if Input.is_action_pressed("ui_left"):
-		velocity.x = -move_speed*delta
+		velocity.x -= move_speed*delta
 		direction = 1;
 		$AnimatedSprite.play("run")
 	elif Input.is_action_pressed("ui_right"):
-		velocity.x = move_speed*delta
+		velocity.x += move_speed*delta
 		direction = -1
 		$AnimatedSprite.play("run")
 	elif grounded:
 		$AnimatedSprite.play("idle")
-	
-	$AnimatedSprite.flip_h = direction == -1;
-	$AnimatedSprite.set_rotation(0)
-	
+		velocity.x = 0
+		
 	# Hook physics
 	if $Tongue.hooked:
-		# `to_local($Chain.tip).normalized()` is the direction that the chain is pulling
-		tongue_velocity = to_local($Tongue.tip).normalized() * TONGUE_PULL
-		if tongue_velocity.y > 0:
-			# Pulling down isn't as strong
-			tongue_velocity.y *= 0.55
-		else:
-			# Pulling up is stronger
-			tongue_velocity.y *= 1.65
-		if sign(tongue_velocity.x) != sign(direction):
-			# if we are trying to walk in a different
-			# direction than the chain is pulling
-			# reduce its pull
-			tongue_velocity.x *= 0.7
-	else:
-		# Not hooked -> no chain velocity
-		tongue_velocity = Vector2(0,0)
-	velocity += tongue_velocity
+		var target_dir = $Tongue/Tip.global_position - global_position;
+		velocity += target_dir.normalized() * TONGUE_PULL * delta;
+		
+	if velocity.x > 0: 
+		direction = -1 
+	else: 
+		direction = 1
+	$AnimatedSprite.flip_h = direction == -1;
+	$AnimatedSprite.set_rotation(0)
 
-	
 	if not grounded:
 		rotate_swog()
 		$AnimatedSprite.play("air")
---
+++ scripts/Tongue.gd
@@ -0,0 +1,43 @@
+extends Node2D
+
+onready var tongue = $TongueLinks;
+
+export(int) var SPEED : int = 20;
+
+var flying : bool = false;
+var hooked : bool = false;
+
+var direction : Vector2 = Vector2()
+var tip_pos : Vector2 = Vector2()
+
+func _input(event):
+	if event is InputEventMouseButton:
+		if event.is_pressed():
+			shoot(get_global_mouse_position() - global_position);
+		else:
+			release()
+
+func shoot(target):
+	flying = true
+	direction = target.normalized()
+	
+func release():
+	flying = false;
+	hooked = false;
+	$Tip.position = Vector2()
+
+func _process(_delta):
+	var tip_dir : float = atan2($Tip.position.y,$Tip.position.x) + PI/2
+	tongue.set_region_rect(Rect2(0,0,8,$Tip.position.length()))
+	tongue.set_rotation(tip_dir+PI)
+	$Tip.set_rotation(tip_dir)
+
+func _physics_process(_delta):
+	if hooked: $Tip.global_position = tip_pos;
+	if not flying: return;
+	
+	if $Tip.move_and_collide(direction*SPEED):
+		hooked = true;
+		flying = false;
+		tip_pos = $Tip.global_position
+		
